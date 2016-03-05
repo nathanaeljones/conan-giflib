6b1699cb2b933c277beb38eb2f856e956bb97d21
Luis Martinez <lasote@gmail.com>
2016-03-05 13:09:56 +0100

    now without the project C

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,6 @@ from conans import ConanFile
 import os, shutil
 from conans.tools import download, unzip, replace_in_file
 from conans import CMake, ConfigureEnvironment
-from cStringIO import StringIO
 
 
 class ZlibNgConan(ConanFile):
@@ -62,14 +61,11 @@ class ZlibNgConan(ConanFile):
         else:
             cmake = CMake(self.settings)
             self.run("cd %s && mkdir _build" % self.ZIP_FOLDER_NAME)
-            self.output.info("Created dir %s/_build" % self.ZIP_FOLDER_NAME)
             cd_build = "cd %s/_build" % self.ZIP_FOLDER_NAME
-            
-            backup_dir = os.getcwd()
-            os.chdir("./%s/_build" % self.ZIP_FOLDER_NAME)
-            self.run('cmake .. %s' % cmake.command_line)
-            self.run("cmake --build . %s" % cmake.build_config)
-            os.chdir(backup_dir)
+            self.output.warn('%s && cmake .. %s' % (cd_build, cmake.command_line))
+            self.run('%s && cmake .. %s' % (cd_build, cmake.command_line))
+            self.output.warn("%s && cmake --build . %s" % (cd_build, cmake.build_config))
+            self.run("%s && cmake --build . %s" % (cd_build, cmake.build_config))
  
     def package(self):
         # Copy FindGIF.cmake to package
